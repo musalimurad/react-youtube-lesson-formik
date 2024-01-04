c4aabc91a6e46145ddfaffab368a5b8866c5f8b9
murad musali <musalimurat1@gmail.com>
2024-01-05 02:33:28 +0400

    youtube-formik-react-login

--- src/App.jsx
+++ src/App.jsx
@@ -1,114 +1,43 @@
-import "./App.css";
-import { useFormik } from "formik";
-function App() {
+import React from 'react'
+import { useFormik } from 'formik'
+import axios from 'axios';
+
+const App = () => {
+  const apiUrl = 'https://fakestoreapi.com/auth/login';
   let {handleChange, handleSubmit, values} = useFormik({
     initialValues:{
-      email:"",
-      password:"",
-      gender:"",
-      hobbies:[],
-      country:""
+      username:"",
+      password:""
     },
-    onSubmit:(values)=>{
+    onSubmit:()=>{
       console.log(values);
+      axios.post(apiUrl, {
+        username:values.username,
+        password:values.password
+      }).then((response)=>{
+        console.log(response.data.token);
+      }).catch((error)=>{
+        console.log(error.response.data);
+      })
     }
   })
   return (
-    <div className="App">
-      <h1>Sign In</h1>
-
+    <div>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="email">Email: </label>
-        <input
-          onChange={handleChange}
-          type="email"
-          placeholder="enter your email..."
-          id="email"
-          name="email"
-        />
+        <label htmlFor="username">Username: </label>
+        <br />
+        <input type="text" placeholder='enter your username...' name='username' id='username' onChange={handleChange} />
         <br />
         <br />
         <label htmlFor="password">Password: </label>
-        <input
-          onChange={handleChange}
-          type="password"
-          name="password"
-          id="password"
-          placeholder="enter your password..."
-        />
+        <br />
+        <input type="password" placeholder='enter your password...' name='password' id='password' onChange={handleChange} />
         <br />
         <br />
-        <div>
-          Gender:
-          <div>
-            <label htmlFor="male">Male</label>
-            <input
-              onChange={handleChange}
-              id="male"
-              name="gender"
-              type="radio"
-              value="male"
-              checked={values.gender === "male"}
-            />
-          </div>
-          <div>
-            <label htmlFor="female">Female</label>
-            <input
-              onChange={handleChange}
-              id="female"
-              name="gender"
-              type="radio"
-              value="female"
-              checked={values.gender === "female"}
-            />
-          </div>
-        </div>
-        <br />
-        <div>
-          Hobbies:
-          <div>
-            <label htmlFor="car">Car</label>
-            <input
-              onChange={handleChange}
-              name="hobbies"
-              id="car"
-              type="checkbox"
-              value="car"
-            />
-          </div>
-          <div>
-            <label htmlFor="code">Coding</label>
-            <input
-              onChange={handleChange}
-              name="hobbies"
-              id="code"
-              type="checkbox"
-              value="coding"
-            />
-          </div>
-          <div>
-            <label htmlFor="travel">Travel</label>
-            <input
-              onChange={handleChange}
-              name="hobbies"
-              id="travel"
-              type="checkbox"
-              value="travel"
-            />
-          </div>
-        </div>
-        <br />
-        <select value={values.country} onChange={handleChange} name="country">
-          <option value="Azerbaijan">Azerbaijan</option>
-          <option value="Turkey">Turkey</option>
-          <option value="United State">United State</option>
-        </select>
-        <br />
-        <br />
-        <input type="submit" value={"login"} />
+        <input type="submit" value="login" />
       </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
